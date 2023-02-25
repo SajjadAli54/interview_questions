22e25ead420a437f5c0b2e894a0a778a8a3b2c11
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2023-02-25 23:18:31 +0500

    Working on ShareDrive problem

--- ShareDrive/app.js
+++ ShareDrive/app.js
@@ -1,5 +1,5 @@
 const id = 10017
-const month = 04
+const month = `04`
 
 function getMonth(date) {
   return date.substr(0, 2)
@@ -28,8 +28,35 @@ const paymentsUrl = 'https://www.jsonkeeper.com/b/9QRZ'
 const ridesPromise = get(ridesUrl)
 const paymentsPromise = get(paymentsUrl)
 Promise.all([ridesPromise, paymentsPromise]).then((data) => {
-  const rides = data[0].filter((ride) => ride['driver_id'] == id)
-  const payments = data[1]
+  const riders = data[0].filter((rider) => rider['driver_id'] == id)
+  const payments = data[1].filter((rider) => rider['driver_id'] == id)
 
-  console.log('Rides: ', rides)
+  const obj = {}
+  riders.forEach((rider) => {
+    const month = getMonth(rider['trip_date'])
+    if (month in obj) obj[month] += rider['trip_details']['fare']
+    else obj[month] = rider['trip_details']['fare']
+  })
+
+  payments.forEach((payment) => {
+    const month = getMonth(payment['date'])
+    if (month in obj) obj[month] += payment['amount']
+    else obj[month] = payment['amount']
+  })
+
+  const keys = Object.keys(obj).sort()
+  let payable = 0.0
+  let total = 0.0
+  let indx = 1
+  if (keys.length > indx) {
+    total = obj[keys[indx++]]
+    payable = 0.1 * total
+  }
+
+  while (indx < keys.length) {
+    total += obj[keys[indx]]
+    payable += 0.2 * obj[keys[indx++]]
+  }
+
+  console.log(`Payable: ${total - payable}`)
 })
